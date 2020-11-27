5f44be523515a27b76afb593d5a0af39e147f715
yfxie <1244843022@qq.com>
2020-11-27 21:43:19 +0800

    祭奠留言、纪念相册

--- src/views/people/people-jidian.vue
+++ src/views/people/people-jidian.vue
@@ -1,72 +1,145 @@
+<!--<template>-->
+<!--  <div class="article">-->
+<!--    <img class="life-icon" alt src="http://www.tiantang6.com/m/moban/1/img/dltt.gif">-->
+
+<!--    <div class="article-list">-->
+<!--      <div-->
+<!--        class="article-item"-->
+<!--        v-for="(item, index) in articles"-->
+<!--        :key="index"-->
+<!--      >-->
+<!--        <h3>签名：{{item.name}}</h3>-->
+<!--        <p>-->
+<!--          <img-->
+<!--            :src="item.img"-->
+<!--            alt=""-->
+<!--            width="90"-->
+<!--            height="60"-->
+<!--            class=""-->
+<!--          >-->
+<!--          {{item.summary}}-->
+<!--        </p>-->
+<!--        <div style="text-align:right">-->
+<!--          <span>{{item.time}}</span>-->
+<!--        </div>-->
+<!--        <a-divider dashed />-->
+<!--      </div>-->
+<!--      <a-pagination-->
+<!--        v-model="current"-->
+<!--        :total="50"-->
+<!--        show-less-items-->
+<!--        style="text-align:center;"-->
+<!--      />-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
 <template>
-  <div class="article">
+  <div style="width: 50%">
     <img class="life-icon" alt src="http://www.tiantang6.com/m/moban/1/img/dltt.gif">
 
-    <div class="article-list">
-      <div
-        class="article-item"
-        v-for="(item, index) in articles"
-        :key="index"
-      >
-        <h3>签名：{{item.name}}</h3>
-        <p>
-          <img
-            :src="item.img"
-            alt=""
-            width="90"
-            height="60"
-            class=""
-          >
-          {{item.summary}}
-        </p>
-        <div style="text-align:right">
-          <span>{{item.time}}</span>
-        </div>
-        <a-divider dashed />
+    <a-list
+      v-if="comments.length"
+      :data-source="comments"
+      :header="`留言数： ${comments.length} `"
+      item-layout="horizontal"
+    >
+
+
+      <a-list-item slot="renderItem" slot-scope="item">
+        <a-comment
+          :author="item.author"
+          :avatar="item.avatar"
+          :content="item.content"
+          :datetime="item.datetime"
+        />
+      </a-list-item>
+    </a-list>
+    <a-comment>
+      <div slot="content">
+        <a-form-item>
+          <a-textarea :rows="4" :value="value" @change="handleChange" placeholder="留言处" style="resize:none;"/>
+        </a-form-item>
+        <a-form-item>
+          <a-button html-type="submit" :loading="submitting" type="primary" @click="handleSubmit">
+            提交
+          </a-button>
+        </a-form-item>
       </div>
-      <a-pagination
-        v-model="current"
-        :total="50"
-        show-less-items
-        style="text-align:center;"
-      />
-    </div>
+    </a-comment>
   </div>
 </template>
+<!--<script>-->
 
+<!--export default {-->
+<!--  name: "people-jidian",-->
+<!--  data() {-->
+<!--    const item = {-->
+<!--      name: "父亲",-->
+<!--      img: "http://www.tiantang6.com/pic/4/15.gif",-->
+<!--      summary:-->
+<!--        "岁岁清明，今又清明，万株杨柳属流莺。千百年来，清明不仅见证了华夏民族的根脉情深，传承了千年古国的文化基因，在漫长的时间中更积蓄了沉甸甸的民俗、文化内涵，已绵延成为中华儿女缅怀先祖、寄托思念的一场几近虔诚与近乎唯美的文化仪式。",-->
+<!--      time: "2019/03/25",-->
+<!--    };-->
+<!--    return {-->
+<!--      articles: Array(5).fill(item),-->
+<!--      current: 2,-->
+<!--    };-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
 <script>
-
+import moment from 'moment';
 export default {
-  name: "people-jidian",
   data() {
-    const item = {
-      name: "父亲",
-      img: "http://www.tiantang6.com/pic/4/15.gif",
-      summary:
-        "岁岁清明，今又清明，万株杨柳属流莺。千百年来，清明不仅见证了华夏民族的根脉情深，传承了千年古国的文化基因，在漫长的时间中更积蓄了沉甸甸的民俗、文化内涵，已绵延成为中华儿女缅怀先祖、寄托思念的一场几近虔诚与近乎唯美的文化仪式。",
-      time: "2019/03/25",
-    };
     return {
-      articles: Array(10).fill(item),
-      current: 2,
+      comments: [
+        {
+          author: 'Han Solo',
+          avatar: 'http://www.tiantang6.com/pic/4/15.gif',
+          content: '一路走好',
+          datetime: moment().fromNow(),
+        },
+        {
+          author: 'Han Solo',
+          avatar: 'http://www.tiantang6.com/pic/4/15.gif',
+          content: '一路走好',
+          datetime: moment().fromNow(),
+        }
+      ],
+      submitting: false,
+      value: '',
+      moment,
+
     };
-  }
-}
+  },
+  methods: {
+    handleSubmit() {
+      if (!this.value) {
+        return;
+      }
+
+      this.submitting = true;
+
+      setTimeout(() => {
+        this.submitting = false;
+        this.comments = [
+          {
+            author: 'Han Solo',
+            avatar: 'http://www.tiantang6.com/pic/4/15.gif',
+            content: this.value,
+            datetime: moment().fromNow(),
+          },
+          ...this.comments,
+        ];
+        this.value = '';
+      }, 1000);
+    },
+    handleChange(e) {
+      this.value = e.target.value;
+    },
+  },
+};
 </script>
-
 <style scoped>
-  .article {
-    width: 50%;
-    padding: 8px;
-  }
 
-  .article-list {
-    padding-left: 16px;
-    padding-right: 16px;
-  }
-
-  .article-item img {
-    float: left;
-    margin-right: 16px;
-  }
 </style>
--- src/views/people/people-picture.vue
+++ src/views/people/people-picture.vue
@@ -1,41 +1,88 @@
 <template>
     <div>
       <img class="life-icon" alt src="http://www.tiantang6.com/m/moban/1/img/pictt.gif">
-      <div style="background-color: #ececec; padding: 20px;">
-        <a-row :gutter="16">
-          <a-col :span="8">
-            <a-card hoverable>
+      <div>
+        <a-upload-list class="uuu">
+          <a-list-item class="lll">
+            <a-card hoverable style="width: 140px">
               <img
-                   alt="example"
-                   class="life-icon"
-                   src="http://localhost:8080/static/images/03.jpg" width="100dp">
-              <template slot="actions" class="ant-card-actions">
-              </template>
+                slot="cover"
+                alt="example"
+                src="http://localhost:8080/static/images/03.jpg"
+              />
+              <a-card-meta title="相册1">
+                <template slot="description">
+                  <a href="/people">点击查看</a>
+                </template>
+              </a-card-meta>
             </a-card>
-          </a-col>
-          <a-col :span="8">
-            <a-card title="Card title" :bordered="false">
-              <img class="life-icon" alt src="http://localhost:8080/static/images/03.jpg" width="100dp">
+          </a-list-item>
+          <a-list-item class="lll">
+            <a-card hoverable style="width: 140px">
+              <img
+                slot="cover"
+                alt="example"
+                src="http://localhost:8080/static/images/03.jpg"
+              />
+              <a-card-meta title="相册1">
+                <template slot="description">
+                  <a href="/people">点击查看</a>
+                </template>
+              </a-card-meta>
             </a-card>
-          </a-col>
-          <a-col :span="8">
-            <a-card title="Card title" :bordered="false">
-              <img class="life-icon" alt src="http://localhost:8080/static/images/03.jpg" width="100dp">
-            </a-card>
-          </a-col>
-        </a-row>
+          </a-list-item>
+        </a-upload-list>
       </div>
+
+
+
+
+<!--      <img class="life-icon" alt src="http://www.tiantang6.com/m/moban/1/img/pictt.gif">-->
+<!--      <div style="background-color: #ececec; padding: 20px;">-->
+<!--        <a-row :gutter="16">-->
+<!--          <a-col :span="8">-->
+<!--            <a-card hoverable>-->
+<!--              <img-->
+<!--                   alt="example"-->
+<!--                   class="life-icon"-->
+<!--                   src="http://localhost:8080/static/images/03.jpg" width="100dp">-->
+<!--              <template slot="actions" class="ant-card-actions">-->
+<!--              </template>-->
+<!--            </a-card>-->
+<!--          </a-col>-->
+<!--          <a-col :span="8">-->
+<!--            <a-card title="Card title" :bordered="false">-->
+<!--              <img class="life-icon" alt src="http://localhost:8080/static/images/03.jpg" width="100dp">-->
+<!--            </a-card>-->
+<!--          </a-col>-->
+<!--          <a-col :span="8">-->
+<!--            <a-card title="Card title" :bordered="false">-->
+<!--              <img class="life-icon" alt src="http://localhost:8080/static/images/03.jpg" width="100dp">-->
+<!--            </a-card>-->
+<!--          </a-col>-->
+<!--        </a-row>-->
+<!--      </div>-->
 
     </div>
 </template>
 
 <script>
-export default {
-  name: "people-photo"
 
+export default {
+  name: "people-picture"
 }
 </script>
 
 <style scoped>
 
+  .uuu {
+    list-style: none;
+    margin-left: 50px;
+  }
+
+  .lll {
+    display: inline;
+    line-height: 40px;
+    float:left
+  }
 </style>
